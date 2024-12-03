384124154ab079da85fb4045b075c65b33770da3
levietducanh99 <ducanhvahung@gmail.com>
2024-12-03 23:13:43 +0700

    delete

--- src/main/java/com/project/ims/repository/ImportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ims.repository;
-
-import com.project.ims.model.entity.Import;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImportRepository extends JpaRepository<Import, Integer> {
-}
